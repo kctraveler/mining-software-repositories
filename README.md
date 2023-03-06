3868558bf40927cf2ad3f7a96f1d88cdcc7631d3
Shane Panchot <shane.panchot@gmail.com>
2023-03-05 20:13:39 -0600

    API connection established

---
+++ README.md
@@ -0,0 +1,21 @@
+# Notes for contributors to this project
+
+## Setting up .ENV variables
+
+- When cloning this repository you will need to create a .env file defining the following variables. Alternatively you can set this as an envionment varaible in your OS.
+- the .env file is part of the .gitignore and won't be committed.
+
+
+| Variable Name | Value |
+| ---           | ---   |
+| GITHUB_TOKEN  | [Personal Access Token from Github](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token) granting public repo access | 
+
+
+## Links to documentation for libraries used in this project
+
+- [PyGithub Documenation](https://pygithub.readthedocs.io/en/latest/index.html)
+- [pyton-dotenv Documentation](https://pypi.org/project/python-dotenv/)
+- [argparse Documenation](https://docs.python.org/3/library/argparse.html#add-help)
+- [logging Documentation](https://docs.python.org/3/library/logging.html)
+
+
---
+++ msr.py
@@ -0,0 +1,47 @@
+import argparse
+import logging
+import os
+from github import Github
+from github import enable_console_debug_logging
+import github
+from dotenv import load_dotenv
+
+
+def main():
+    # Handle command line arguments
+    parser = argparse.ArgumentParser(
+        description="Extracts data about a GitHub repository",
+        prog="Mining Software Repositories")
+    parser.add_argument('--repo',
+                        action='store',
+                        default="scikit-learn/scikit-learn",
+                        help="Full repository name: owner/repo")
+    parser.add_argument('--loglevel',
+                        action='store',
+                        default='INFO',
+                        choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'])
+    args = parser.parse_args()
+
+    # Setup logging
+    logging.basicConfig(level=getattr(logging, args.loglevel.upper()),
+                        format='%(asctime)s %(levelname)s: %(message)s')
+    if logging.root.level <= 10:
+        enable_console_debug_logging()
+        logging.debug("PyGithub logging enabled.")
+
+    # Establish API connection and get Repo
+    try:
+        load_dotenv()
+        gh = Github(os.environ['GITHUB_TOKEN'])
+        repo = gh.get_repo(args.repo)
+        logging.info(
+            "API connection established and repo returned with ID %d", repo.id)
+    except KeyError as e:
+        raise Exception(
+            "Github Token not defined in .env or system enviornment variable") from None
+    except Exception as e:
+        logging.critical("Error returning repository: %s", e)
+
+
+if __name__ == '__main__':
+    main()
