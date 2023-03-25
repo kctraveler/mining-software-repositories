0c2c272aec5003cf8c0baf9b51fed3e98106e12a
Pragya Kansal <pragyakansal419@gmail.com>
2023-03-25 16:46:40 -0500

    Made changes to code_size

--- msr_code_size.py
+++ msr_code_size.py
@@ -1,6 +1,5 @@
 def get_code_size(commit):
     tree_elements = commit.commit.tree.tree
-    print(tree_elements[0])
     tree_size = 0
     for tree_element in tree_elements:
         if tree_element.size:
